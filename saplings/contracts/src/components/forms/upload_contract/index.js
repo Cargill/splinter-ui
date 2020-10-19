7b65ac62ea11fa996f74d2ced7beabf181d7b81e
Nitish Poddar <Nitish_Poddar@cargill.com>
2020-10-19 11:36:24 -0500

    Create basic UI for upload contract form
    Fix package.json merge conflict
    
    Functionality for uploading files
    
    Resolved merged conflict, previously not dealt with
    
    Changed Public Key, Added useEffect Argument
    
    Start support for get circuits
    
    Signed-off-by: Yi Shian Ho <yishian_ho@cargill.com>
    
    Signed-off-by: Nitish Poddar <Nitish_Poddar@cargill.com>

--- saplings/contracts/src/components/forms/upload_contract/index.js
+++ saplings/contracts/src/components/forms/upload_contract/index.js
@@ -1,21 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { MultiStepForm, Step } from 'App/components/forms/MultiStepForm';
+import { ListBoxSelect, TextField, FileSelect } from 'App/components/forms/controls';
+import { createCallPayload, listCircuits, getCircuit } from '../../../api/splinter';
+// import { ListBoxSelect, TextField } from '../controls'
+
+import Checkbox from 'rc-checkbox';
+import 'rc-checkbox/assets/index.css';
 
 import './index.scss';
 
+const NamespaceForm = () => {
+  const [readState, setReadState] = useState(false);
+  const [writeState, setWriteState] = useState(false);
+
+  return (
+    <div>
+      <TextField name='Namespace Registry Name' label='Namespace Registry Name' />
+      <span style={{marginTop: '5px'}} />
+      <ListBoxSelect label='Select Owners of the Namespace' 
+        name='abcd' onChange={(event) => console.log(event)} options={[
+        { value: 'User 1', content: 'User 1' },
+        { value: 'User 2', content: 'User 2' },
+        { value: 'User 3', content: 'User 3' }
+        ]} isMulti={true} />
+      <span className='label' style={{marginLeft: '16px', marginTop: '5px'}}>Set Permissions for the Namespace</span>
+      <div style={{flexDirection: 'row', marginTop: '10px' }}>
+        <Checkbox checked={readState} onChange={() => setReadState(!readState)} 
+          style={{margin: '0 15px'}} /> Read
+        <Checkbox checked={writeState} onChange={() => setWriteState(!writeState)} 
+          style={{margin: '0 0 0 15px'}} /> Write
+      </div>
+    </div>
+  )
+}
+
 export function UploadContractForm() {
+  const [registries, setRegistries] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFilename, setSelectedFilename] = useState("");
+  const [buffer, setBuffer] = useState(null);
+  const [contractRegistryName, setContractRegistryName] = useState("");
+
+  const onFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+    if (event.target.files[0]) {
+      setSelectedFilename(event.target.files[0].name);
+    }
+  }
+
+  useEffect(() => {
+    if (buffer) {
+      console.log(buffer);
+      createCallPayload(new Uint8Array(buffer));
+    }
+  }, [buffer]);
+    
+
+  useEffect(() => {
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(selectedFile);
+      reader.onload = () => {
+        setBuffer(reader.result);
+      }
+    }
+  }, [selectedFile]);
+
+  useEffect(async () => {
+    const res = await listCircuits();
+    console.log(res);
+    const circuitId = res['data'][0]['id']
+    const circuitData = await getCircuit(circuitId);
+    console.log(circuitData);
+  }, []);
+
   return (
     <MultiStepForm
       formName="Upload Contract"
       handleSubmit={() => { console.log('Next step'); }}
       handleCancel={() => { console.log('Cancel') }}>
-      <Step step={1} label="Select circuit">
+      <Step step={1} label="Select Circuit">
         <div className="step-header">
           <div className="step-title">Select Circuit</div>
           <div className="help-text">
             Select the circuit to deploy the smart contract to.
           </div>
+        </div>
+        <div>
+          <ListBoxSelect label='Select Circuit' name='abcd' onChange={(event) => console.log(event)} options={[
+            { value: 'Circuit ID 1', content: 'Circuit ID 1' },
+            { value: 'Circuit ID 2', content: 'Circuit ID 2' }
+            ]} />
         </div>
       </Step>
       <Step step={2} label="Upload contract">
@@ -25,6 +101,13 @@ export function UploadContractForm() {
             Upload the packaged smart contract in the form of a .scar file.
           </div>
         </div>
+        <div>
+          <FileSelect name='Upload Contract' label='Upload Contract'
+            onChange={onFileChange} />
+          <span style={{marginTop: '5px'}} />
+          <TextField name='Contract Registry Name' label='Contract Registry Name' value={contractRegistryName}
+            onChange={(evt) => {setContractRegistryName(evt.target.value)}} />
+        </div>
       </Step>
       <Step step={3} label="Create Namespace Registry">
         <div className="step-header">
@@ -33,13 +116,8 @@ export function UploadContractForm() {
             Create the namespace registry for the uploaded contract.
           </div>
         </div>
-      </Step>
-      <Step step={4} label="Set contract permissions">
-        <div className="step-header">
-          <div className="step-title">Set Contract Permissions</div>
-          <div className="help-text">
-            Set the relevant permissions for the contract.
-          </div>
+        <div>
+          <NamespaceForm />
         </div>
       </Step>
     </MultiStepForm>
